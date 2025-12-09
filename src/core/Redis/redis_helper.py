fabc71f6b52a6cb961c88baf791e27e41dc07359
eztoen <ikucheren0510@gmail.com>
2025-12-09 19:14:31 +0600

    Created redis helper for get client and close connection

---
+++ src/core/Redis/redis_helper.py
@@ -0,0 +1,19 @@
+from redis.asyncio import Redis
+from core.config import settings
+
+class RedisHelper:
+    def __init__(self):
+        self.redis = Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            db=settings.REDIS_DB,
+            decode_responses=True
+        )
+        
+    async def get_client(self) -> Redis:
+        return self.redis
+    
+    async def close(self):
+        await self.redis.close()
+        
+redis_helper = RedisHelper()
